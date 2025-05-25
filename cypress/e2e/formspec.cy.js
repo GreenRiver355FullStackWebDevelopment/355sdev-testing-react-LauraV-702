9b4a10dbaa4165c7ce8be12c467d589ffc3eb400
LauraV702 <dsfjsdfjlsdk@student.greenriver.edu>
2025-05-25 16:19:18 -0700

    Deliverable 5 completed

--- cypress/e2e/formspec.cy.js
+++ cypress/e2e/formspec.cy.js
@@ -3,6 +3,12 @@ describe("Restaurant Rating Form Tests", () => {
     cy.visit("http://localhost:3000/"); 
   });
 
-  it("Creates a booking", () => {
+  it("Fills out and submits the rating form", () => {
+    cy.get('[data-cy="input-name"]').type('Pizza House');
+    cy.get('[data-cy="input-address"]').type('123r PL NE, Seattle, USA');
+    cy.get('[data-cy="input-phone"]').type('111-123-1234');
+    cy.get('[data-cy="input-cuisine"]').type('Italian');
+    cy.get('[data-cy="input-rating"]').type('5');
+
   });
 });
